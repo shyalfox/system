4eb19512c9bfc65415b0dc8a3aa7daff20766111
Sanish_Bal <sbalnet369@gmail.com>
2022-03-29 11:22:02 +0545

    first commit

---
+++ login.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form method=post action="db/login.php">
+            <label>Email Address</label>
+            <input type="text" name="email" placeholder="Enter email adress"></input>
+            <label>Password </label>
+            <input type="text" name="password" placeholder="Enter Password"></input>
+
+            <input type="submit" value="Login"></input>
+
+    </body>
+</html>
