aea5d5c94ea9b7f3459e91a88cda4725a8dde6f4
Rahul <mtal3584@gmail.com>
2022-03-30 22:27:17 +0545

    login added

--- db/login.php
+++ db/login.php
@@ -1,1 +1,34 @@
-<!-- login php -->
+<?php
+  include('connect.php');
+  if(isset($_POST['login'])){
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+
+      if($email==''){
+          $msg = "email is required";
+          header('Location:../login.php?errmsg='+$msg);
+      }
+      if($password==''){
+          $msg = "password is required";
+          header('Location:../login.php?errmsg='+$msg);
+      }
+    $encryptedPass = md5($password);  
+    $query = "SELECT * FROM customers WHERE email = '$email' AND password = '$encryptedPass'";
+    $result = mysqli_query($conn,$query);
+    
+    $row = mysqli_num_rows($result);
+    if($row==1){
+        $data = mysqli_fetch_assoc($result);
+        session_start();
+        $_SESSION['login']=1;
+        $_SESSION['user_id']= $data['id'];
+
+        header('Location:../home.php');
+    }else{
+        header('Location:../home.php?errmsg=email and password does not match');
+    }
+
+  }
+
+?>
+
